daec029b9b8514c0b4fb4640c97574d6017a94b1
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-27 17:33:01 +0530

    [Madhavi] create user register api and login api

---
+++ FundooNoteApp/FundooNoteApp/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooNoteApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBL iuserBL;
+
+        public UserController(IUserBL iuserBL)
+        {
+            this.iuserBL = iuserBL;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+
+        public IActionResult RegisterUser(UserRegistrationModel userRegistration)
+        {
+            try
+            {
+                var result = iuserBL.Registration(userRegistration);
+                if(result != null)
+                {
+                    return Ok(new { success = true, message = "Registration Successful", data = result});
+                }
+                else 
+                { 
+                    return BadRequest(new { success = false, message = "Registration Unsuccessful" }); 
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult UserLogin(UserLoginModel userLogin)
+        {
+            try
+            {
+                var result = iuserBL.Login(userLogin);
+                if (result != null)
+                {
+                    return Ok(new { success = true, message = "Login Successful", data = result });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Login Unsuccessful" });
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
